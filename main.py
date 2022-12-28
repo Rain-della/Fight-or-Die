25d6a0d73aaa054374d049ec00820ae0616c6645
King-Rain <112505657+Rain-della@users.noreply.github.com>
2022-12-28 17:06:20 +0100

    Trying to make a menu screen

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pygame
+import pygame_menu
+from pygame_menu import themes
 
 #! i made a 2 projectile classes for each player to try and fix the clumping of the bullets with player 2
 #! IT didnt work but i am going to leave it there because i feel it looks better like that
-#TODO make a main menu screen and a pause function/screen for the game
+# TODO make a main menu screen and a pause function/screen for the game
 
 
 # initializes every command
@@ -16,6 +18,29 @@ pygame.display.set_caption("Fight Or Die")
 clock = pygame.time.Clock()
 score = pygame.font.SysFont("arial", 50, True)
 # The player1 class with  all the player1 attributes and functions
+
+
+def set_difficulty(value, difficulty):
+    print(value)
+    print(difficulty)
+
+
+def start_the_game():
+    mainmenu._open(loading)
+    pygame.time.set_timer(update_loading, 30)
+
+
+mainmenu = pygame_menu.Menu("Welcome", 800, 500, theme=themes.THEME_SOLARIZED)
+mainmenu.add.text_input("Name:", default="username", maxchar=20)
+mainmenu.add.button("Play", start_the_game)
+mainmenu.add.button("Quit", pygame_menu.events.EXIT)
+
+
+loading = pygame_menu.Menu("Loading The Game", 800, 500, theme=themes.THEME_DARK)
+loading.add.progress_bar("Progress", progressbar_id="1", default=0, width=200)
+update_loading = pygame.USEREVENT + 0
+
+arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size=(10, 15))
 
 
 class P1(object):
@@ -112,6 +137,12 @@ def redrawgamewindow():
         dagger.draw(win)
     for bullet in bullets:
         bullet.draw(win)
+    if mainmenu.is_enabled():
+        mainmenu.update(events)
+        mainmenu.draw(win)
+        if mainmenu.get_current().get_selected_widget():
+            arrow.draw(win, mainmenu.get_current().get_selected_widget())
+
     pygame.display.update()
 
 
@@ -143,7 +174,13 @@ while run:
     if player2.health <= 0:
         player2.reset()
 
-    for event in pygame.event.get():
+    events = pygame.event.get()
+    for event in events:
+        if event.type == update_loading:
+            progress = loading.get_widget("1")
+            progress.set_value(progress.get_value() + 1)
+            if progress.get_value() == 100:
+                pygame.time.set_timer(update_loading, 0)
         if event.type == pygame.QUIT:
             run = False
 
